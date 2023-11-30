d39f2fa6b36c20e8a2bd3bc216f7efa4d28cf5d5
Hans Amelang <hank1313@aol.com>
2023-11-30 17:52:11 -0600

    simplified band.sh, added instrument entry

--- band.sh
+++ band.sh
@@ -3,7 +3,8 @@
 new_band() {
 
 	# array for holding band name and memmber names
-	declare -a band
+	#declare -a members
+	#declare -a instruments
 
 	# exit condition for while loop
 	quit="n"
@@ -11,21 +12,25 @@ new_band() {
 	# intro, prompt
 	echo -en "\e\033[0;33mEnter a new band name: \033[0m"
 	read band_name
-	band+=("$band_name")
+	echo -n $band_name: >> temp_band
+	
 	while [ $quit == "n" ]; do 
 		echo -en "\e\033[0;33mAdd band member (\"q\" to quit adding):\033[0m "
 		read band_member
 		if [ "$band_member" == "q" ]; then
 			let quit="y"
 		else 
-			band+=("$band_member")
+			echo -n "$band_member, " >> temp_band
+			echo -en "\e\033[0;33mEnter their instrument/role in the band:\033[0m "
+			read instrument
+			echo -n $instrument: >> temp_band
 		fi
 	done
 	#echo "${band[@]}"
 
-	for each in "${band[@]}"; do
-		echo -n "$each:" >> temp_band
-	done
+	#for each in "${band[@]}"; do
+	#	echo -n "$each:" >> temp_band
+	#done
 	
 	truncate -s -1 temp_band
 	cat temp_band >> band_list
